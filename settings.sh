a0280c9ffc84bb97c2b2b7b543fae55f8232eeac
Amitabh Prasad <amitabhprasad@Amitabhs-MacBook-Pro-2.local>
2020-10-26 11:57:35 +0530

    init

---
+++ settings.sh
@@ -0,0 +1,16 @@
+echo "Setting operator env"
+export GO111MODULE=on
+
+export APP_NAME=gramophone
+export OPERATOR_NAME=${APP_NAME}-operator
+export OPERATOR_IMAGE=${OPERATOR_NAME}-image
+
+export ORGANIZATION=atarazana
+export DOMAIN=${ORGANIZATION}.com
+export API_VERSION=${APP_NAME}.${DOMAIN}/v1
+
+export PROJECT_NAME=${OPERATOR_NAME}-system
+
+export USERNAME=prasadamitabh
+
+export VERSION=0.0.1
